d99baed1112736651b74ee631d0c3a465f6e33ef
rajpanchall <91491797+rajpanchall@users.noreply.github.com>
2022-09-28 17:33:33 +0530

    Add files via upload

---
+++ Netflix clone/Netflix.js
@@ -0,0 +1,18 @@
+console.log("Hello Netflix!");
+
+
+// const signInbtn = document.getElementById("sign-In-btn");
+
+// signInbtn.addEventListener(SignInfunction = () => {
+//     const newSigninpage = document.getElementById('newSigninpage');
+//     newSigninpage.style.display = block;
+// });
+
+
+var html = document.getElementById('newSigninpage');
+html.addEventListener('onclick', SignInfunction = () => {
+
+    html.style.display = 'block';
+    console.log("yes!🎈")
+
+})
